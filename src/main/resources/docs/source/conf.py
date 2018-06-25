86a6c9017620fb9fac360e03db5f0e65cbcfc62a
Raimund Hocke <sikulix@outlook.com>
2018-06-25 09:36:26 +0200

    problem import extension

--- src/main/resources/docs/source/conf.py
+++ src/main/resources/docs/source/conf.py
@@ -29,7 +29,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = ['sikulidirectives', 'platformdirective', 'youtube']
-extensions = ['sikulidirectives']
+#extensions = ['sikulidirectives']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
